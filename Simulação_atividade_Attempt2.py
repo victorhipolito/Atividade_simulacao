411d6bc8cbccebd53166cc8e8a3d46ba04abacee
victorhipolito <victorhipolito@hotmail.com.br>
2021-07-29 18:27:06 -0300

    Função de geração a cada rodada finalizada, classe pronta.

--- Simulação_atividade_Attempt2.py
+++ Simulação_atividade_Attempt2.py
@@ -17,26 +17,32 @@ from func_xtra import modulo, dis_vet
 # Essa classe será responsável por gerar os termos, desde o mapa até os alimentos e seres.
 class Geracao:
     def __init__(self):
-        # Area do mapa
+        # Area do mapa.
         self.area = int(input("Qual será a área do seu mapa?\n(Valor X Valor)\n _ "))
-        # While que checa se a quantidade de comida, de verdes e vermelhos é menor que a área do mapa
+        # While que checa se a quantidade de comida, de verdes e vermelhos é menor que a área do mapa.
         while True:
             self.qt_comida = int(input("Quantas comidas serão geradas no mapa?\n _ "))
             self.qt_verdes = int(input("Quantos verdes serão gerados no mapa?\n _ "))
             self.qt_vermelhos = int(input("Quantos vermelhos serão gerados no mapa?\n _ "))
-            if self.qt_vermelhos + self.qt_verdes + self.qt_comida < self.area:
+            if self.qt_vermelhos + self.qt_verdes + self.qt_comida <= self.area**2:
                 break
             else:
                 print("Você colocou valores que excedem o limite do mapa, tente novamente")
         self.taxa_alimentos = int(input("De quantas em quantas rodadas um alimento novo será gerado?\n _ "))
-        # Variável de rodada e arrays de todos os termos do mapa juntos, uma apenas de alimentos e outra apenas de seres.
+        # Variável de rodada e array de todos os termos do mapa juntos, uma apenas de alimentos e outra apenas de seres.
         self.rodada = 1
         self.tudo = list()
         self.seres = list()
         self.alimento = list()
+        # Geração de tudo com base nas variáveis de input.
+        self.gerar_posicao(self.seres, tipo="Ser", qt=self.qt_verdes, vida=3, ser="Verde")
+        self.gerar_posicao(self.seres, tipo="Ser", qt=self.qt_vermelhos, vida=3, ser="Vermelho")
+        self.gerar_posicao(self.alimento, qt=self.qt_comida)
 
-
+    # Função que gera posições, com apenas um parâmetro obrigatório, que é a array onde vão ser adicionados os termos.
+    # De padrão, essa função funciona para alimentos mas é possível gerar seres com a mesma.
     def gerar_posicao(self, array, tipo="Alimento", qt=1, vida=None, ser=None):
+        # If para geração de alimentos.
         if tipo == "Alimento":
             for _ in range(0, qt):
                 check = True
@@ -56,6 +62,7 @@ class Geracao:
                         array.append([pos])
                         check = False
 
+        # If para geração de seres.
         elif tipo == "Ser":
             for _ in range(0, qt):
                 check = True
@@ -74,3 +81,9 @@ class Geracao:
                         self.tudo.append([ser, pos])
                         array.append([ser, vida, 0.0, pos])
                         check = False
+
+    # Função que usa a taxa de geração de alimentos como base para gerar em um intervalo definido de rodadas.
+    def geracao_rodadas(self):
+        if self.rodada % self.taxa_alimentos == 0:
+            self.gerar_posicao(self.alimento)
+
